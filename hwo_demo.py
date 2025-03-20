84b624b8888f8f82b4a393228cfb1c3a9675b862
Annika Salmi <annikaksalmi@gmail.com>
2025-03-20 20:46:34 +0000

    [#9] HWO plots

---
+++ hwo_demo.py
@@ -0,0 +1,65 @@
+import requests
+import os
+import lifesim
+import matplotlib.pyplot as plt
+import pandas as pd
+import numpy as np
+
+from lifesim.core.hwo_data import HWOData
+
+# ---------- Set-Up ----------
+
+# create bus
+bus = lifesim.Bus()
+
+# setting the options
+bus.data.options.set_scenario('baseline')
+
+# set options manually
+bus.data.options.set_manual(diameter=4.)
+bus.data.options.set_manual(output_path='data_creation/')
+bus.data.options.set_manual(output_filename='test_runs_0')
+
+# ---------- Downloading the P-Pop catalog ----------
+
+data = requests.get('https://raw.githubusercontent.com/kammerje/P-pop/main/TestPlanetPopulation.txt')
+
+if os.path.isdir('data_creation'):
+    pass
+else:
+    os.makedirs('data_creation')
+if os.path.exists(os.path.join('data_creation','ppop_catalog.txt')):
+    pass
+else:
+    with open(os.path.join('data_creation','ppop_catalog.txt'), 'wb') as file:
+        file.write(data.content)
+
+# ---------- Loading the Catalog ----------
+
+bus.data.catalog_from_ppop(input_path='data_creation/ppop_catalog.txt')
+bus.data.catalog_remove_distance(stype='A', mode='larger', dist=0.)  # remove all A stars
+bus.data.catalog_remove_distance(stype='M', mode='larger', dist=10.)  # remove M stars > 10pc to
+# speed up calculation
+
+hwo_data = HWOData(bus.data)
+hwo_data.determine_detectable()
+
+df_size = hwo_data.catalog.groupby(['stype','habitable']).size().reset_index()
+df_size['count_overall'] = df_size[0]
+df_size =df_size.drop([0], axis=1)
+
+x = np.arange(len(df_size.stype.unique())) 
+
+df_hab = df_size[df_size.habitable==True].drop(['habitable'],axis=1)
+df_false = df_size[df_size.habitable==False].drop(['habitable'],axis=1)
+width=0.4
+
+plt.bar(x-0.2, df_hab.count_overall, width)#, color='cyan') 
+plt.bar(x+0.2, df_false.count_overall, width)#, color='orange') 
+plt.xticks(x, df_size.stype.unique()) 
+plt.xlabel("Stellar Type") 
+plt.ylabel("Count") 
+plt.title('HWO Detectability')
+plt.legend(["Habitable", "Inhabitable"]) 
+plt.show() 
+a=1
---
+++ lifesim/core/hwo_data.py
@@ -0,0 +1,57 @@
+import sys
+import warnings
+
+import numpy as np
+import pandas as pd
+from astropy.io import fits
+from astropy.coordinates import SkyCoord, BarycentricMeanEcliptic
+
+from lifesim.util.options import Options
+from lifesim.util.habitable import single_habitable_zone
+from lifesim.core.data import Data
+
+
+# TODO: automatically add data storage for all
+class HWOData(Data):
+    """
+    The data class is the central storage class for catalogs, options, parameters and data. Any
+    data used in simulations should be stored in this class. Via the bus, access to the data class
+    is given to all modules.
+
+    Attributes
+    ----------
+    inst : dict
+        Data used for simulation of the instrument.
+    catalog : pd.DataFrame
+        Catalog containing all exoplanets in the sample.
+    single : dict
+        Data used for the spectral simulation of single exoplanets.
+    other : dict
+        Data storage for any other pertinent data.
+    options : Options
+        Location of the Options class. All options and free parameters used in a LIFEsim simulation
+        must be stored here.
+    """
+    def __init__(self, Data):
+        super().__init__()
+        self.catalog=Data.catalog
+        self.IWA = 62e-6
+        self.planet_flux_star_ratio = 10e-10
+        self.flux_ratio = self.calc_flux()
+        self.iwa_constraint = self.calc_iwa_constraint()
+
+    def calc_flux(self):
+        flux_ratio = (self.catalog.radius_p.values / self.catalog.radius_s.values)**2 * \
+                            self.catalog.temp_p.values / self.catalog.temp_s.values
+        return flux_ratio
+    
+    def calc_iwa_constraint(self):
+        iwa_constraint = self.catalog.sep_p.values /self.catalog.distance_s
+        return iwa_constraint
+    
+    def determine_detectable(self):
+        iwa_condition = self.iwa_constraint <= self.IWA
+        flux_condition = self.flux_ratio >= self.flux_ratio
+        total_condition = iwa_condition & flux_condition
+        self.catalog['hwo_detectable'] = total_condition
+        return self.catalog.hwo_detectable

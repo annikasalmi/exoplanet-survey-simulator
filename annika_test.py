2903276a078c5c9b5347025a34214daa28468ffb
Annika Salmi <annikaksalmi@gmail.com>
2025-05-04 09:29:46 +0100

    [#16] tbd if this is a bug or just the way it is; will wait and find out

--- annika_test.py
+++ annika_test.py
@@ -1,17 +1,30 @@
 import pandas as pd
 import numpy as np
 
-for i in range(3):
-    stype = ['A', 'B', 'C', 'D']
-    count_overall = [np.random.randint(0, 100) for _ in range(4)]
-    df = pd.DataFrame(data={'stype': stype, 'count_overall': count_overall})
-    
-    temp_dict = dict(np.asarray(df))
+from PPop.PlanetDistributions.SAG13 import PlanetDistribution
+from PPop.Star import Star
 
-    if i == 0:
-        total_dict = temp_dict
-    else:
-        for key in temp_dict:
-            list_dict = list([total_dict[key]])
-            total_dict[key]= list_dict.append(temp_dict[key])
-            a=1
+PlanetDistribution = PlanetDistribution(Scenario='baseline')
+Star = Star(
+    Name='TestStar',
+    Dist=1.0,
+    Stype='G',
+    Rad=1.0,
+    Teff=5778,
+    Mass=1.0,
+    RA=180.0,
+    Dec=0.0,
+    Vmag=5.0,
+    Jmag=None,
+    Hmag=None,
+    WDSsep=None,
+    WDSdmag=None,
+    lGal=None,
+    bGal=None
+)
+
+r_p_all = []
+for i in range(500):
+    n_planets = np.random.randint(1, 6)
+    r_p, _ = PlanetDistribution.draw(Star=Star, Nplanets=n_planets)
+    r_p_all.extend(r_p)
